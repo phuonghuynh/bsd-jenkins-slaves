d0f18f9e2b03685c77ad7ecf6b6f46c8a00d3897
phuonghqh <phuonghqh@gmail.com>
2017-09-30 15:40:13 +0700

    Add freebsd private key

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 pkg install -y openjdk8-jre
 
+mv /tmp/scripts/jenkins-slave /usr/local/etc/rc.d/jenkins-slave
+
+su vagrant
+
 mkdir -p /usr/share/jenkins
-mv /tmp/slave.jar /usr/share/jenkins/
-mv /tmp/slave.sh /usr/share/jenkins/slave.sh
+wget https://repo.jenkins-ci.org/public/org/jenkins-ci/main/remoting/3.1/remoting-3.1.jar --output-document=/usr/share/slave.jar
+
+# mv /tmp/slave.jar /usr/share/jenkins/
+
